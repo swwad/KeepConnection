75f9fb35b324929709ec42803208679a6c288957
Darren_Home <swwad1@gmail.com>
2014-02-05 01:37:14 +0800

    update code

--- src/com/monster/keepconnection/SettingActivity.java
+++ src/com/monster/keepconnection/SettingActivity.java
@@ -1,32 +1,26 @@
 package com.monster.keepconnection;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.preference.CheckBoxPreference;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceGroup;
+import android.telephony.TelephonyManager;
 import android.widget.Toast;
 
-import com.monster.keepconnection.R;
 import com.util.IabHelper;
-import com.util.IabHelper.OnIabPurchaseFinishedListener;
-import com.util.IabHelper.QueryInventoryFinishedListener;
-import com.util.IabResult;
-import com.util.Inventory;
-import com.util.Purchase;
 
 public class SettingActivity extends PreferenceActivity implements OnPreferenceChangeListener, OnPreferenceClickListener {
 
@@ -44,40 +38,9 @@ public class SettingActivity extends PreferenceActivity implements OnPreferenceC
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		BluetoothAdapter.getDefaultAdapter().enable();
-		hCheckBtDeviceStatus.sendEmptyMessage(0);
 		addPreferencesFromResource(R.xml.setting_preference);
 		reStartService();
 
-		if (!getSharedPreferences(getPackageName(), MODE_PRIVATE).getBoolean(getString(R.string.key_full_version), false)) {
-			releaseIabHelper();
-			if (mHelper == null)
-				mHelper = new IabHelper(this, IABKey);
-			mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
-				public void onIabSetupFinished(IabResult result) {
-					if (!result.isSuccess()) {
-						releaseIabHelper();
-						ToastUiThread(SettingActivity.this, getString(R.string.iabhelper_failed), Toast.LENGTH_SHORT);
-						return;
-					}
-					mHelper.queryInventoryAsync(new QueryInventoryFinishedListener() {
-						@Override
-						public void onQueryInventoryFinished(IabResult result, Inventory inv) {
-							if (result.isFailure()) {
-								releaseIabHelper();
-								ToastUiThread(SettingActivity.this, getString(R.string.iabhelper_failed), Toast.LENGTH_SHORT);
-								return;
-							}
-							if (inv.getPurchase(FullVersionID) != null) {
-								getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putBoolean(getString(R.string.key_full_version), true).commit();
-								ToastUiThread(SettingActivity.this, getString(R.string.iabhelper_fullversion), Toast.LENGTH_LONG);
-								hSetupDefaultData.sendEmptyMessage(0);
-								releaseIabHelper();
-							}
-						}
-					});
-				}
-			});
-		}
 	}
 
 	@Override
@@ -85,39 +48,6 @@ public class SettingActivity extends PreferenceActivity implements OnPreferenceC
 		super.onResume();
 		hSetupDefaultData.sendEmptyMessage(0);
 	}
-
-	Handler hCheckBtDeviceStatus = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			super.handleMessage(msg);
-			if (BluetoothAdapter.getDefaultAdapter() != null) {
-				pairedDevices = BluetoothAdapter.getDefaultAdapter().getBondedDevices();
-				ListPreference listPref = (ListPreference) findPreference(getString(R.string.key_setting_select_btdevice));
-				if (pairedDevices.size() > 0) {
-					listPref.setSummary("");
-					String[] entryDeviceValues = new String[pairedDevices.size()];
-					String[] entryDevice = new String[pairedDevices.size()];
-					int i = 0;
-					for (BluetoothDevice device : pairedDevices) {
-						entryDeviceValues[i] = device.getAddress();
-						entryDevice[i] = device.getName();
-						if (getSharedPreferences(getPackageName(), MODE_PRIVATE).getString(getString(R.string.pref_setting_bt_device_address), "").equalsIgnoreCase(device.getAddress()))
-							listPref.setSummary(device.getName());
-						i++;
-					}
-					listPref.setEntries(entryDevice);
-					listPref.setEntryValues(entryDeviceValues);
-					listPref.setOnPreferenceChangeListener(SettingActivity.this);
-					listPref.setEnabled(true);
-					hCheckBtDeviceStatus.removeCallbacksAndMessages(null);
-				} else {
-					listPref.setEnabled(false);
-					hCheckBtDeviceStatus.sendMessageDelayed(new Message().obtain(), 500);
-				}
-			}
-
-		}
-	};
 
 	Handler hSetupDefaultData = new Handler() {
 		@Override
@@ -128,141 +58,55 @@ public class SettingActivity extends PreferenceActivity implements OnPreferenceC
 	};
 
 	void SetDefaultData() {
-		String[] OptionString = getResources().getStringArray(R.array.warning_option_string);
-		String[] OptionValue = getResources().getStringArray(R.array.warning_option_value);
-
-		CheckBoxPreference cbPref = (CheckBoxPreference) findPreference(getString(R.string.key_setting_auto_start));
-		cbPref.setChecked(getSharedPreferences(getPackageName(), MODE_PRIVATE).getBoolean(getString(R.string.pref_setting_auto_start), false));
-		cbPref.setOnPreferenceChangeListener(this);
-		cbPref.setEnabled(getSharedPreferences(getPackageName(), MODE_PRIVATE).getBoolean(getString(R.string.key_full_version), false));
 
 		Preference pref = (Preference) findPreference(getString(R.string.key_setting_support_me));
-		if (getSharedPreferences(getPackageName(), MODE_PRIVATE).getBoolean(getString(R.string.key_full_version), false)) {
-			if (pref != null)
-				((PreferenceGroup) findPreference(getString(R.string.key_setting_title))).removePreference(pref);
-		} else {
-			pref.setOnPreferenceClickListener(this);
-		}
+		pref.setOnPreferenceClickListener(this);
 
-		ListPreference listPref = (ListPreference) findPreference(getString(R.string.key_notify_audio));
-		for (int i = 0; i < OptionValue.length; i++) {
-			if (getSharedPreferences(getPackageName(), MODE_PRIVATE).getString(getString(R.string.pref_warning_audio), "0").equalsIgnoreCase(OptionValue[i])) {
-				listPref.setSummary(OptionString[i]);
-				break;
-			}
-		}
-		listPref.setOnPreferenceChangeListener(this);
-		listPref.setEnabled(true);
-
-		listPref = (ListPreference) findPreference(getString(R.string.key_notify_vibrate));
-		for (int i = 0; i < OptionValue.length; i++) {
-			if (getSharedPreferences(getPackageName(), MODE_PRIVATE).getString(getString(R.string.pref_warning_vibrator), "0").equalsIgnoreCase(OptionValue[i])) {
-				listPref.setSummary(OptionString[i]);
-				break;
-			}
-		}
-		listPref.setOnPreferenceChangeListener(this);
-		listPref.setEnabled(true);
-
-		listPref = (ListPreference) findPreference(getString(R.string.key_notify_flash));
-		for (int i = 0; i < OptionValue.length; i++) {
-			String sss = getSharedPreferences(getPackageName(), MODE_PRIVATE).getString(getString(R.string.pref_warning_flash), "0");
-			if (getSharedPreferences(getPackageName(), MODE_PRIVATE).getString(getString(R.string.pref_warning_flash), "0").equalsIgnoreCase(OptionValue[i])) {
-				listPref.setSummary(OptionString[i]);
-				break;
-			}
-		}
-		listPref.setOnPreferenceChangeListener(this);
-		if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
-			listPref.setEnabled(getSharedPreferences(getPackageName(), MODE_PRIVATE).getBoolean(getString(R.string.key_full_version), false));
-		} else {
-			listPref.setEnabled(false);
-			getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putString(getString(R.string.pref_warning_flash), "0").commit();
-		}
-
-		cbPref = (CheckBoxPreference) findPreference(getString(R.string.key_notify_screen));
-		cbPref.setChecked(getSharedPreferences(getPackageName(), MODE_PRIVATE).getBoolean(getString(R.string.pref_warning_screen), false));
-		cbPref.setOnPreferenceChangeListener(this);
-		cbPref.setEnabled(getSharedPreferences(getPackageName(), MODE_PRIVATE).getBoolean(getString(R.string.key_full_version), false));
-
-		cbPref = (CheckBoxPreference) findPreference(getString(R.string.key_notify_popwindow));
-		cbPref.setChecked(getSharedPreferences(getPackageName(), MODE_PRIVATE).getBoolean(getString(R.string.pref_warning_popwindow), false));
-		cbPref.setOnPreferenceChangeListener(this);
-		cbPref.setEnabled(getSharedPreferences(getPackageName(), MODE_PRIVATE).getBoolean(getString(R.string.key_full_version), false));
 	}
 
 	@Override
 	public boolean onPreferenceChange(Preference preference, Object newValue) {
-		if (getString(R.string.key_setting_select_btdevice).equals(preference.getKey())) {
-			getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putString(getString(R.string.pref_setting_bt_device_address), (String) newValue).commit();
 
-			for (BluetoothDevice device : pairedDevices) {
-				if (getSharedPreferences(getPackageName(), MODE_PRIVATE).getString(getString(R.string.pref_setting_bt_device_address), "").equalsIgnoreCase(device.getAddress())) {
-					((ListPreference) findPreference(getString(R.string.key_setting_select_btdevice))).setSummary(device.getName());
-					getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putString(getString(R.string.pref_setting_bt_device_name), device.getName()).commit();
-					break;
-				}
-			}
-			reStartService();
-		} else if (getString(R.string.key_setting_auto_start).equals(preference.getKey())) {
-			getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putBoolean(getString(R.string.pref_setting_auto_start), (Boolean) newValue).commit();
-			reStartService();
-		} else if (getString(R.string.key_notify_audio).equals(preference.getKey())) {
-			getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putString(getString(R.string.pref_warning_audio), (String) newValue).commit();
-		} else if (getString(R.string.key_notify_flash).equals(preference.getKey())) {
-			getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putString(getString(R.string.pref_warning_flash), (String) newValue).commit();
-		} else if (getString(R.string.key_notify_popwindow).equals(preference.getKey())) {
-			getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putBoolean(getString(R.string.pref_warning_popwindow), (Boolean) newValue).commit();
-		} else if (getString(R.string.key_notify_screen).equals(preference.getKey())) {
-			getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putBoolean(getString(R.string.pref_warning_screen), (Boolean) newValue).commit();
-		} else if (getString(R.string.key_notify_vibrate).equals(preference.getKey())) {
-			getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putString(getString(R.string.pref_warning_vibrator), (String) newValue).commit();
-		}
-		hSetupDefaultData.sendEmptyMessage(0);
 		return true;
 	}
-
+	
+	boolean aaa = false;
+	
 	@Override
 	public boolean onPreferenceClick(Preference preference) {
 		if (getString(R.string.key_setting_support_me).equals(preference.getKey())) {
-			releaseIabHelper();
-			if (mHelper == null)
-				mHelper = new IabHelper(this, IABKey);
+			ToastUiThread(this, "PP", Toast.LENGTH_SHORT);
 
-			mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
-				public void onIabSetupFinished(IabResult result) {
-					if (!result.isSuccess()) {
-						releaseIabHelper();
-						ToastUiThread(SettingActivity.this, getString(R.string.iabhelper_failed), Toast.LENGTH_SHORT);
-					} else {
-						mHelper.launchPurchaseFlow(SettingActivity.this, FullVersionID, BuyFullVersionRequestCode, new OnIabPurchaseFinishedListener() {
-							@Override
-							public void onIabPurchaseFinished(IabResult result, Purchase info) {
-								if (result.isFailure()) {
-									if (result.getResponse() == IabHelper.BILLING_RESPONSE_RESULT_ITEM_ALREADY_OWNED) {
-										getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putBoolean(getString(R.string.key_full_version), true).commit();
-										ToastUiThread(SettingActivity.this, getString(R.string.iabhelper_fullversion), Toast.LENGTH_LONG);
-									} else {
-										getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putBoolean(getString(R.string.key_full_version), false).commit();
-										ToastUiThread(SettingActivity.this, result.getMessage(), Toast.LENGTH_SHORT);
-									}
-								} else if ((result.isSuccess()) && (info.getSku().equals(FullVersionID))) {
-									getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putBoolean(getString(R.string.key_full_version), true).commit();
-									ToastUiThread(SettingActivity.this, getString(R.string.iabhelper_fullversion), Toast.LENGTH_LONG);
-								} else {
-									getSharedPreferences(getPackageName(), MODE_PRIVATE).edit().putBoolean(getString(R.string.key_full_version), false).commit();
-									ToastUiThread(SettingActivity.this, result.getMessage(), Toast.LENGTH_SHORT);
-								}
-								releaseIabHelper();
-								hSetupDefaultData.sendEmptyMessage(0);
-							}
-						}, IABRequestCode);
-					}
-				}
-			});
+			aaa = !aaa;
+			
+			Method dataConnSwitchmethod;
+			Class telephonyManagerClass;
+			Object ITelephonyStub;
+			Class ITelephonyClass;
+
+			TelephonyManager telephonyManager = (TelephonyManager) SettingActivity.this.getSystemService(Context.TELEPHONY_SERVICE);
+
+			try {
+				updateAPN(this, aaa);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return true;
 	}
+	
+	private static void updateAPN(Context paramContext, boolean enable) {
+	    try {
+	        ConnectivityManager connectivityManager = (ConnectivityManager) paramContext.getSystemService("connectivity");
+	        Method setMobileDataEnabledMethod = ConnectivityManager.class.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
+	        setMobileDataEnabledMethod.setAccessible(true);
+	        setMobileDataEnabledMethod.invoke(connectivityManager, enable);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+
 
 	public void ToastUiThread(final Context context, final String strMessage, final int duration) {
 		runOnUiThread(new Runnable() {
@@ -285,11 +129,16 @@ public class SettingActivity extends PreferenceActivity implements OnPreferenceC
 	}
 
 	public void reStartService() {
-		stopService(new Intent(SettingActivity.this, MonitorDeviceService.class));
-		if (getSharedPreferences(getPackageName(), MODE_PRIVATE).getString(getString(R.string.pref_setting_bt_device_address), "").length() == 0) {
-			Toast.makeText(SettingActivity.this, R.string.warning_no_btdevice, Toast.LENGTH_SHORT).show();
-		} else {
-			startService(new Intent(SettingActivity.this, MonitorDeviceService.class).putExtra(StartFromActivity, true));
-		}
+		// stopService(new Intent(SettingActivity.this,
+		// MonitorDeviceService.class));
+		// if (getSharedPreferences(getPackageName(),
+		// MODE_PRIVATE).getString(getString(R.string.pref_setting_bt_device_address),
+		// "").length() == 0) {
+		// Toast.makeText(SettingActivity.this, R.string.warning_no_btdevice,
+		// Toast.LENGTH_SHORT).show();
+		// } else {
+		// startService(new Intent(SettingActivity.this,
+		// MonitorDeviceService.class).putExtra(StartFromActivity, true));
+		// }
 	}
 }
