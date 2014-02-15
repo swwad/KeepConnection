9dd60656f3610fff92ff5d727e05946b15a7de32
Darren <swwad1@gmail.com>
2014-02-15 21:29:31 +0800

    將狀態機放入Service，並改以thread執行

--- src/com/monster/keepconnection/MonitorDeviceService.java
+++ src/com/monster/keepconnection/MonitorDeviceService.java
@@ -1,15 +1,15 @@
 package com.monster.keepconnection;
 
 import java.lang.reflect.Method;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
-import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
@@ -18,12 +18,16 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
 import android.os.SystemClock;
 import android.os.Vibrator;
-import android.view.WindowManager;
+import android.widget.Toast;
 
 enum InternetStatus {
 	None, Mobile_Data_Failed, Mobile_Data_Off, Wifi_Data_Failed, Wifi_Data_Off, AIR_PLANE, Internet_Unknow, Internet_Failed, Inter_Success;
@@ -35,32 +39,13 @@ public class MonitorDeviceService extends Service {
 	Camera camera;
 	Parameters camera_parameters;
 	int iBackCameraID = -1;
+
 	int iRetryCounter = 0;
 	boolean bStopAllWarning = false;
+	boolean bMonitorServiceThread = true;;
 	boolean bInternetStatus = false;
 	InternetStatus isStatus = InternetStatus.None;
-
-	
-	private static void updateAPN(Context paramContext, boolean enable) {
-		try {
-			ConnectivityManager connectivityManager = (ConnectivityManager) paramContext.getSystemService("connectivity");
-			Method setMobileDataEnabledMethod = ConnectivityManager.class.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
-			setMobileDataEnabledMethod.setAccessible(true);
-			setMobileDataEnabledMethod.invoke(connectivityManager, enable);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private boolean isConnectingToInternet() {
-		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		if (connMgr != null) {
-			NetworkInfo info = connMgr.getActiveNetworkInfo();
-			if (info != null)
-				return (info.isConnected());
-		}
-		return false;
-	}
+	int iLastStatus = -1;
 
 	@Override
 	public void onCreate() {
@@ -77,11 +62,13 @@ public class MonitorDeviceService extends Service {
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		try {
-			if (getSharedPreferences(getPackageName(), MODE_PRIVATE).getBoolean(getString(R.string.pref_setting_auto_start), false) || intent.getBooleanExtra(SettingActivity.StartFromActivity, false)) {
+			if (getSharedPreferences(getPackageName(), MODE_PRIVATE).getBoolean(getString(R.string.key_setting_auto_start), false) || intent.getBooleanExtra(SettingActivity.StartFromActivity, false)) {
 				bStopAllWarning = false;
 				iRetryCounter = 0;
 				isStatus = InternetStatus.None;
 				showNotification();
+				bMonitorServiceThread = true;
+				thStateMachine.start();
 			} else {
 				stopSelf();
 			}
@@ -92,23 +79,130 @@ public class MonitorDeviceService extends Service {
 		return super.onStartCommand(intent, flags, startId);
 	}
 
-	public void warningDialog() {
-		Intent dialogIntent = new Intent(getBaseContext(), SettingActivity.class);
-		dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		getApplication().startActivity(dialogIntent);
+	Handler hToastMessage = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			Toast.makeText(MonitorDeviceService.this, msg.getData().getString("Message"), Toast.LENGTH_LONG).show();
+		}
+	};
 
-		AlertDialog.Builder builder = new AlertDialog.Builder(MonitorDeviceService.this);
-		builder.setTitle(R.string.app_name);
-		builder.setMessage(String.format(getString(R.string.warning_dialog), getSharedPreferences(getPackageName(), MODE_PRIVATE).getString(getString(R.string.pref_setting_bt_device_name), "")));
-		builder.setPositiveButton(R.string.option_ok, new OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				bStopAllWarning = true;
+	Message setStringMessage(String strMessage) {
+		Bundle bData = new Bundle();
+		// bData.putString("Message", "Inter_Success or None");
+		bData.putString("Message", strMessage);
+		Message msMessage = new Message().obtain();
+		msMessage.setData(bData);
+		return msMessage;
+	}
+
+	Thread thStateMachine = new Thread(new Runnable() {
+		@Override
+		public void run() {
+			while (bMonitorServiceThread) {
+				if (isConnectingToInternet() == false) {
+					switch (isStatus) {
+					case Inter_Success:
+					case None:
+						hToastMessage.sendMessage(setStringMessage("Inter_Success or None"));
+						isStatus = InternetStatus.Internet_Failed;
+						break;
+					case Internet_Failed:
+						hToastMessage.sendMessage(setStringMessage("Internet_Failed"));
+						if (getConnectType() == ConnectivityManager.TYPE_MOBILE) {
+							isStatus = InternetStatus.Mobile_Data_Failed;
+						} else if (getConnectType() == ConnectivityManager.TYPE_WIFI) {
+							isStatus = InternetStatus.Wifi_Data_Failed;
+						} else {
+							isStatus = InternetStatus.Internet_Unknow;
+						}
+						break;
+					case Internet_Unknow:
+						hToastMessage.sendMessage(setStringMessage("Internet_Unknow"));
+						((WifiManager) getSystemService(Context.WIFI_SERVICE)).setWifiEnabled(false);
+						if (getConnectType() != ConnectivityManager.TYPE_MOBILE) {
+							updateAPN(MonitorDeviceService.this, true);
+							isStatus = InternetStatus.None;
+						}
+						break;
+					case Mobile_Data_Failed:
+						hToastMessage.sendMessage(setStringMessage("Mobile_Data_Failed"));
+						updateAPN(MonitorDeviceService.this, false);
+						isStatus = InternetStatus.Mobile_Data_Off;
+						break;
+					case Wifi_Data_Failed:
+						hToastMessage.sendMessage(setStringMessage("Wifi_Data_Failed"));
+						((WifiManager) getSystemService(Context.WIFI_SERVICE)).setWifiEnabled(false);
+						isStatus = InternetStatus.Wifi_Data_Off;
+						break;
+					case Mobile_Data_Off:
+						hToastMessage.sendMessage(setStringMessage("Mobile_Data_Off"));
+						if (getConnectType() != ConnectivityManager.TYPE_MOBILE) {
+							updateAPN(MonitorDeviceService.this, true);
+							isStatus = InternetStatus.None;
+						}
+						break;
+					case Wifi_Data_Off:
+						hToastMessage.sendMessage(setStringMessage("Wifi_Data_Off"));
+						if (getConnectType() != ConnectivityManager.TYPE_WIFI) {
+							((WifiManager) getSystemService(Context.WIFI_SERVICE)).setWifiEnabled(true);
+							isStatus = InternetStatus.None;
+						}
+						break;
+					default:
+					}
+				} else {
+					isStatus = InternetStatus.Inter_Success;
+					iLastStatus = getConnectType();
+					hToastMessage.sendMessage(setStringMessage("Inter_Success"));
+				}
+				System.gc();
+				try {
+					Thread.sleep(10000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
-		});
-		final AlertDialog dialog = builder.create();
-		dialog.getWindow().setType((WindowManager.LayoutParams.TYPE_SYSTEM_ALERT));
-		dialog.show();
+		}
+
+	}, "KeepConnection_StateMachine_Thread");
+
+	boolean isConnectingToInternet() {
+		boolean bRET = false;
+		try {
+			URLConnection connection = new URL("http://www.google.com.tw").openConnection();
+			connection.setConnectTimeout(5 * 1000);
+			if (((HttpURLConnection) connection).getResponseCode() == HttpURLConnection.HTTP_OK) {
+				bRET = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			bRET = false;
+		}
+		return bRET;
+	}
+
+	void updateAPN(Context paramContext, boolean enable) {
+		try {
+			ConnectivityManager connectivityManager = (ConnectivityManager) paramContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+			Method setMobileDataEnabledMethod = ConnectivityManager.class.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
+			setMobileDataEnabledMethod.setAccessible(true);
+			setMobileDataEnabledMethod.invoke(connectivityManager, enable);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	int getConnectType() {
+		int iType = -1;
+		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (connMgr != null) {
+			NetworkInfo info = connMgr.getActiveNetworkInfo();
+			if (info != null) {
+				iType = info.getType();
+			}
+		}
+		return iType;
 	}
 
 	public void warningVibrator(final int iSec) {
@@ -196,15 +290,13 @@ public class MonitorDeviceService extends Service {
 		Notification notification = new Notification(R.drawable.ic_launcher, getString(R.string.run_in_service), System.currentTimeMillis());
 		// notification.flags = Notification.FLAG_NO_CLEAR;
 		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, MonitorDeviceService.class), 0);
-		notification.setLatestEventInfo(this, getString(R.string.run_in_service),
-				getString(R.string.connecting_device) + getSharedPreferences(getPackageName(), MODE_PRIVATE).getString(getString(R.string.pref_setting_bt_device_name), ""), contentIntent);
+		notification.setLatestEventInfo(this, getString(R.string.run_in_service), getString(R.string.iabhelper_fullversion), contentIntent);
 		mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 		mNM.notify(R.string.app_name, notification);
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 }
--- src/com/monster/keepconnection/SettingActivity.java
+++ src/com/monster/keepconnection/SettingActivity.java
@@ -1,18 +1,11 @@
 package com.monster.keepconnection;
 
-import java.lang.reflect.Method;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.HashSet;
 import java.util.Set;
 
-import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.wifi.WifiManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -36,19 +29,12 @@ public class SettingActivity extends PreferenceActivity implements OnPreferenceC
 
 	IabHelper mHelper;
 
-	int iRetryCounter = 0;
-	boolean bStopAllWarning = false;
-	boolean bInternetStatus = false;
-	InternetStatus isStatus = InternetStatus.None;
-	int iLastStatus = -1;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		BluetoothAdapter.getDefaultAdapter().enable();
+		setContentView(R.layout.activity_setting);
 		addPreferencesFromResource(R.xml.setting_preference);
-		reStartService();
-
+		getListView().setItemsCanFocus(true);
 	}
 
 	@Override
@@ -78,173 +64,13 @@ public class SettingActivity extends PreferenceActivity implements OnPreferenceC
 		return true;
 	}
 
-	boolean aaa = false;
-
 	@Override
 	public boolean onPreferenceClick(Preference preference) {
 		if (getString(R.string.key_setting_support_me).equals(preference.getKey())) {
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					synchronized (this) {
-						if (isConnectingToInternet() == false) {
-							switch (isStatus) {
-							case Inter_Success:
-							case None:
-								ToastUiThread(SettingActivity.this, "Inter_Success or None", Toast.LENGTH_LONG);
-								isStatus = InternetStatus.Internet_Failed;
-								break;
-							case Internet_Failed:
-								ToastUiThread(SettingActivity.this, "Internet_Failed", Toast.LENGTH_LONG);
-								if (getConnectType() == ConnectivityManager.TYPE_MOBILE) {
-									isStatus = InternetStatus.Mobile_Data_Failed;
-								} else if (getConnectType() == ConnectivityManager.TYPE_WIFI) {
-									isStatus = InternetStatus.Wifi_Data_Failed;
-								} else {
-									isStatus = InternetStatus.Internet_Unknow;
-								}
-								break;
-							case Internet_Unknow:
-								ToastUiThread(SettingActivity.this, "Internet_Unknow", Toast.LENGTH_LONG);
-								((WifiManager) getSystemService(Context.WIFI_SERVICE)).setWifiEnabled(false);
-								if (getConnectType() != ConnectivityManager.TYPE_MOBILE) {
-									updateAPN(SettingActivity.this, true);
-									isStatus = InternetStatus.None;
-								}
-								break;
-							case Mobile_Data_Failed:
-								ToastUiThread(SettingActivity.this, "Mobile_Data_Failed", Toast.LENGTH_LONG);
-								updateAPN(SettingActivity.this, false);
-								isStatus = InternetStatus.Mobile_Data_Off;
-								break;
-							case Wifi_Data_Failed:
-								ToastUiThread(SettingActivity.this, "Wifi_Data_Failed", Toast.LENGTH_LONG);
-								((WifiManager) getSystemService(Context.WIFI_SERVICE)).setWifiEnabled(false);
-								isStatus = InternetStatus.Wifi_Data_Off;
-								break;
-							case Mobile_Data_Off:
-								ToastUiThread(SettingActivity.this, "Mobile_Data_Off", Toast.LENGTH_LONG);
-								if (getConnectType() != ConnectivityManager.TYPE_MOBILE) {
-									updateAPN(SettingActivity.this, true);
-									isStatus = InternetStatus.None;
-								}
-								break;
-							case Wifi_Data_Off:
-								ToastUiThread(SettingActivity.this, "Wifi_Data_Off", Toast.LENGTH_LONG);
-								if (getConnectType() != ConnectivityManager.TYPE_WIFI) {
-									((WifiManager) getSystemService(Context.WIFI_SERVICE)).setWifiEnabled(true);
-									isStatus = InternetStatus.None;
-								}
-								break;
-							default:
-							}
-						} else {
-							isStatus = InternetStatus.Inter_Success;
-							iLastStatus = getConnectType();
-							ToastUiThread(SettingActivity.this, "Inter_Success", Toast.LENGTH_LONG);
-						}
-						System.gc();
-					}
-				}
-			}, "KeepConnection_StateMachine_Thread").start();
+
+			reStartService();
 		}
 		return true;
-	}
-
-	Handler hCheckInternetStatus = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			super.handleMessage(msg);
-			if (isConnectingToInternet() == false) {
-
-				if (iRetryCounter > 3) {
-					iRetryCounter = 0;
-
-					switch (isStatus) {
-					case Inter_Success:
-					case None:
-						isStatus = InternetStatus.Internet_Failed;
-						break;
-					case Internet_Failed:
-						if (getConnectType() == ConnectivityManager.TYPE_MOBILE) {
-							isStatus = InternetStatus.Mobile_Data_Failed;
-						} else if (getConnectType() == ConnectivityManager.TYPE_WIFI) {
-							isStatus = InternetStatus.Wifi_Data_Failed;
-						}
-						break;
-					case Mobile_Data_Failed:
-						updateAPN(SettingActivity.this, false);
-						isStatus = InternetStatus.Mobile_Data_Off;
-						break;
-					case Wifi_Data_Failed:
-						((WifiManager) getSystemService(Context.WIFI_SERVICE)).setWifiEnabled(false);
-						isStatus = InternetStatus.Wifi_Data_Off;
-						break;
-					case Mobile_Data_Off:
-						if (getConnectType() != ConnectivityManager.TYPE_MOBILE) {
-							updateAPN(SettingActivity.this, true);
-							isStatus = InternetStatus.None;
-						}
-						break;
-					case Wifi_Data_Off:
-						if (getConnectType() != ConnectivityManager.TYPE_WIFI) {
-							((WifiManager) getSystemService(Context.WIFI_SERVICE)).setWifiEnabled(true);
-							isStatus = InternetStatus.None;
-						}
-						break;
-					default:
-
-					}
-
-				} else {
-					iRetryCounter++;
-				}
-				hCheckInternetStatus.sendMessageDelayed(new Message().obtain(), 10000);
-			} else {
-				iRetryCounter = 0;
-				isStatus = InternetStatus.Inter_Success;
-				hCheckInternetStatus.sendMessageDelayed(new Message().obtain(), 30000);
-			}
-
-		}
-	};
-
-	void updateAPN(Context paramContext, boolean enable) {
-		try {
-			ConnectivityManager connectivityManager = (ConnectivityManager) paramContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-			Method setMobileDataEnabledMethod = ConnectivityManager.class.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
-			setMobileDataEnabledMethod.setAccessible(true);
-			setMobileDataEnabledMethod.invoke(connectivityManager, enable);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	boolean isConnectingToInternet() {
-		boolean bRET = false;
-		try {
-			URLConnection connection = new URL("http://www.google.com.tw").openConnection();
-			connection.setConnectTimeout(5 * 1000);
-			if (((HttpURLConnection) connection).getResponseCode() == HttpURLConnection.HTTP_OK) {
-				bRET = true;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			bRET = false;
-		}
-		return bRET;
-	}
-
-	int getConnectType() {
-		int iType = -1;
-		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		if (connMgr != null) {
-			NetworkInfo info = connMgr.getActiveNetworkInfo();
-			if (info != null) {
-				iType = info.getType();
-			}
-		}
-		return iType;
 	}
 
 	public void ToastUiThread(final Context context, final String strMessage, final int duration) {
@@ -268,16 +94,7 @@ public class SettingActivity extends PreferenceActivity implements OnPreferenceC
 	}
 
 	public void reStartService() {
-		// stopService(new Intent(SettingActivity.this,
-		// MonitorDeviceService.class));
-		// if (getSharedPreferences(getPackageName(),
-		// MODE_PRIVATE).getString(getString(R.string.pref_setting_bt_device_address),
-		// "").length() == 0) {
-		// Toast.makeText(SettingActivity.this, R.string.warning_no_btdevice,
-		// Toast.LENGTH_SHORT).show();
-		// } else {
-		// startService(new Intent(SettingActivity.this,
-		// MonitorDeviceService.class).putExtra(StartFromActivity, true));
-		// }
+		stopService(new Intent(SettingActivity.this, MonitorDeviceService.class));
+		startService(new Intent(SettingActivity.this, MonitorDeviceService.class).putExtra(StartFromActivity, true));
 	}
 }
